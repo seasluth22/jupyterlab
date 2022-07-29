7159438e698688163b6e0899ad0dd385d8db956e
foxtreekr <foxtreekr@naver.com>
2022-07-29 22:43:59 +0900

    first commit

---
+++ module1.py
@@ -0,0 +1,10 @@
+# Module1.py
+## 함수 선언 부분
+def func1() :
+    print('Module1.py의 func1()이 호출됨')
+
+def func2() :
+    print('Module1.py의 func1()이 호출됨')
+
+def func3() :
+    print('Module1.py의 func1()이 호출됨')
